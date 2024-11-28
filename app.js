e3e2b406793f5a501daea7fb523108fd41d635f3
Rj Lemmor Maglaqui <rjlemmormaglaqui@gmail.com>
2024-11-27 18:56:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express'); 
+const app = express();
+const PORT = 8000 || 8080
+const jwt = require('jsonwebtoken');
+
+//route to issue a json web token
+app.post('/user/generateToken/', (req, res) => {
+    
+    const secret_key = 'supersecretkey';
+    const user = {
+        time: new Date(),
+        id: 15,
+    }
+
+    const token = jwt.sign(user, secret_key);
+    res.send(token);
+});
+
+app.get('/user/validateToken', (req, res) => {
+    
+    const secret_key = 'supersecretkey';
+    const token = req.header.authorization.split(' ')[1];
+
+    try {
+        const verified = jwt.verify(token, secret_key);
+
+        if(verified){
+            res.json("User succefully verified.");
+        } else {
+            res.status(401).send("User not authorized."); 
+        }
+
+    } catch(error) {
+        res.status(500).send("Internal Server Error."); 
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Connected to port ${PORT}`);
+});
